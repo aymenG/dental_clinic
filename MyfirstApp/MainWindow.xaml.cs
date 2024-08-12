9086b74b5ed50ae21148db56cf26c218ed55c002
aymen gasmi <aymengasmi25@gmail.com>
2024-08-12 09:51:04 +0100

    Ajoutez des fichiers projet.

---
+++ MyfirstApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyfirstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    /*    private void textEmail_MouseDown(object sender, RoutedEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+        private void txtEmail_TextChanged(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+
+                textEmail.Visibility = Visibility.Collapsed;
+
+            }
+            else {
+
+                textEmail.Visibility = Visibility.Visible;
+
+
+            }
+
+        }
+        private void textPassword_MouseDown(object sender, RoutedEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+
+                txtPassword.Visibility = Visibility.Collapsed;
+
+            }
+            else
+            {
+
+                txtPassword.Visibility = Visibility.Visible;
+
+
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }*/
+    }
+}
+
