dc4303582db57cdaea32eb8e33d36c0698bc9190
aymen gasmi <aymengasmi25@gmail.com>
2024-08-18 16:11:10 +0100

    add logo and adjust login interface

--- MyfirstApp/MainWindow.xaml.cs
+++ MyfirstApp/MainWindow.xaml.cs
@@ -18,77 +18,18 @@ namespace MyfirstApp
     {
         public MainWindow()
         {
-            InitializeComponent();
+         
+        }
+
+    
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrEmpty(txtEmail.Text) && !String.IsNullOrEmpty(txtPassword.Password))
-            {
-                MessageBox.Show("Successfully Signed In");
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
-
-        }
-
-        private void textEmail_MouseDown(object sender, RoutedEventArgs e)
-        {
-            txtEmail.Focus();
-        }
-        private void txtEmail_TextChanged(object sender, RoutedEventArgs e)
-        {
-            if (!String.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
-            {
-
-                textEmail.Visibility = Visibility.Collapsed;
-
-            }
-            else
-            {
-
-                textEmail.Visibility = Visibility.Visible;
-
-
-            }
-
-        }
-        private void textPassword_MouseDown(object sender, RoutedEventArgs e)
-        {
-            txtPassword.Focus();
-        }
-        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            if (!String.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
-            {
-
-                txtPassword.Visibility = Visibility.Collapsed;
-
-            }
-            else
-            {
-
-                txtPassword.Visibility = Visibility.Visible;
-
-
-            }
-
-        }
-
-        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            Application.Current.Shutdown();
 
         }
     }
